a844ed1f7b294d80cf67d42f8136008991b31f4a
Cristian <94758438+Cristi2396@users.noreply.github.com>
2021-12-23 23:42:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*window.setTimeout(function () {
+  window.location.reload(1);
+}, 600);*/
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+            menu.classList.toggle('is-active');
+            menuLinks.classList.toggle('active');
+        });
